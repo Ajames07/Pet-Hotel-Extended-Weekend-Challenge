da72596a62c54009f4b3e46a5b765f7f5f9504e7
Ajames07 <1silent0tter@gmail.com>
2018-08-22 17:07:00 -0500

    added basic functionality to server.js

---
+++ server/public/scripts/client.js
@@ -0,0 +1,8 @@
+console.log('JS');
+
+let app = angular.module('PetHotelApp', []);
+
+app.controller('HotelController', function() {
+    console.log('HotelController loaded');
+    const vm = this;
+})
