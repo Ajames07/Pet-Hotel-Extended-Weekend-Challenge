92c783a822b7402beffbad76ca9e70aafc417331
Ajames07 <1silent0tter@gmail.com>
2018-08-23 13:53:26 -0500

    sourced in angular-route, added route paths to client.js, added form and inputs to dashboard and owner.html

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,21 @@
 console.log('JS');
 
-let app = angular.module('PetHotelApp', []);
+let app = angular.module('PetHotelApp', ['ngRoute']);
 
-app.controller('HotelController', function() {
-    console.log('HotelController loaded');
-    const vm = this;
-})
+app.config(['$routeProvider', function ($routeProvider) {
+    console.log('Route config loaded');
+    
+    $routeProvider
+    .when('/', {
+        redirectTo: '/dashboard'
+    })
+    .when('/dashboard', {
+        templateUrl: 'views/dashboard.html',
+        controller: 'DashBoardController as vm'
+    })
+    .when('/owners', {
+        templateUrl: 'views/owners.html',
+        controller: 'OwnersController as vm'
+    })
+    .otherwise( { template: '<h1>404</h1>' } );
+}]);
